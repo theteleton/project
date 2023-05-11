78d87bc47660c6dd336a990f2162c79b0500b91f
theteleton <it8816@student.uni-lj.si>
2023-05-11 18:47:20 +0200

    Scraped the first visualization.

--- src/crawler.py
+++ src/crawler.py
@@ -2,13 +2,20 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 
 report_id = '745405bd-c702-481b-9e5d-bb3dfc1cc93d'
 group_id = '418fc146-6f6a-4b64-afc8-d8856a0d5b6f'
 # Set up driver
+
 driver = webdriver.Chrome("./chromedriver")
 driver.get(f"https://app.powerbi.com/groups/{group_id}/reports/{report_id}/ReportSection")
+driver.maximize_window()
 
 time.sleep(5)
 WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.ID, "email")))
@@ -38,28 +45,17 @@ code_input.send_keys(code)
 driver.find_element_by_id("idSubmit_SAOTCC_Continue").click()
 time.sleep(10)
 driver.find_element_by_id("idSIButton9").click()
+time.sleep(10)
+
+
+actions = ActionChains(driver)
+actions.move_by_offset(949, 128).click().perform()
 time.sleep(5)
-# Wait for Power BI dashboard to load and navigate to the report
-WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.XPATH, "//div[@class='tile-title' and text()='Pair1_Report_1']")))
+driver.find_element_by_xpath('//*[@id="5"]').click()
 
-report = driver.find_element_by_xpath("//div[@class='tile-title' and text()='Pair1_Report1']")
-report.click()
+time.sleep(5)
+driver.find_element_by_xpath('//*[@id="mat-dialog-0"]/export-data-dialog/mat-dialog-actions/button[1]').click()
 
-# Wait for report to load and export data from first visual
-WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.XPATH, "//div[@class='visual-container-repeat']//canvas")))
-
-visual = driver.find_element_by_xpath("//div[@class='visual-container-repeat']//canvas")
-driver.execute_script("arguments[0].scrollIntoView();", visual)
-
-WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.XPATH, "//span[@title='Export data']")))
-
-export_button = driver.find_element_by_xpath("//span[@title='Export data']")
-export_button.click()
-
-WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.XPATH, "//button[@class='control exportDataDialog-exportButton control-enabled']")))
-
-export_data_button = driver.find_element_by_xpath("//button[@class='control exportDataDialog-exportButton control-enabled']")
-export_data_button.click()
-
+time.sleep(10)
 # Close the browser
 driver.quit()
