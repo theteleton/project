80cd1384a55e0b7bed7e38a02174b2f8571de03c
theteleton <it8816@student.uni-lj.si>
2023-05-12 01:20:12 +0200

    Solved some bugs, and the scraper is added.

--- src/crawler.py
+++ src/crawler.py
@@ -9,53 +9,129 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 
-report_id = '745405bd-c702-481b-9e5d-bb3dfc1cc93d'
-group_id = '418fc146-6f6a-4b64-afc8-d8856a0d5b6f'
+
 # Set up driver
+class Crawler:
+    def __init__(self, username, password, group_id, report_id):
+        self.group_id = group_id
+        self.report_id = report_id
+        self.username = username
+        self.password = password
+    
+    def crawl(self):
+        chrome_options = webdriver.ChromeOptions()
+        prefs = {'download.prompt_for_download': False,
+         'download.directory_upgrade': True,
+         'safebrowsing.enabled': False}
+        chrome_options.add_experimental_option("prefs", prefs)
+        chrome_options.add_argument("--disable-extensions")
+        chrome_options.add_argument("--disable-popup-blocking")
+        driver = webdriver.Chrome("./chromedriver", chrome_options=chrome_options)
+        driver.get(f"https://app.powerbi.com/groups/{self.group_id}/reports/{self.report_id}/ReportSection")
+        driver.maximize_window()
 
-driver = webdriver.Chrome("./chromedriver")
-driver.get(f"https://app.powerbi.com/groups/{group_id}/reports/{report_id}/ReportSection")
-driver.maximize_window()
+        time.sleep(5)
+        WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.ID, "email")))
+        email = driver.find_element_by_id("email")
+        email.send_keys(self.username)
+        driver.find_element_by_id("submitBtn").click()
+        time.sleep(5)
+        password = driver.find_element_by_id("i0118")
+        password.send_keys(self.password)
+        driver.find_element_by_id("idSIButton9").click()
+        time.sleep(5)
+        driver.find_element_by_xpath('//*[@id="idDiv_SAOTCS_Proofs"]/div[1]/div').click()
+        time.sleep(5)
+        WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.ID, "idTxtBx_SAOTCC_OTC")))
+        code = input("Enter two-factor authentication code: ")
+        code_input = driver.find_element_by_id("idTxtBx_SAOTCC_OTC")
+        code_input.send_keys(code)
+        driver.find_element_by_id("idSubmit_SAOTCC_Continue").click()
+        time.sleep(10)
+        driver.find_element_by_id("idSIButton9").click()
+        time.sleep(10)
 
-time.sleep(5)
-WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.ID, "email")))
-# Enter email and password
-email = driver.find_element_by_id("email")
-email.send_keys("pbi@in516ht.com")
-driver.find_element_by_id("submitBtn").click()
-time.sleep(5)
+        actions = ActionChains(driver)
+        actions.move_by_offset(961, 121).click().perform()
+        time.sleep(5)
+        driver.find_element_by_xpath('//*[@id="5"]').click()
+        time.sleep(5)
+        driver.find_element_by_xpath('//*[@id="mat-dialog-0"]/export-data-dialog/mat-dialog-actions/button[1]').click()
+        time.sleep(5)
 
-password = driver.find_element_by_id("i0118")
-password.send_keys("7rTMTgw#BFBPR*WU")
-driver.find_element_by_id("idSIButton9").click()
+        actions = ActionChains(driver)
+        actions.move_by_offset(0, 120).click().perform()
+        time.sleep(5)
+        driver.find_element_by_xpath('//*[@id="5"]').click()
+        time.sleep(5)
+        driver.find_element_by_xpath('//*[@id="mat-dialog-1"]/export-data-dialog/mat-dialog-actions/button[1]').click()
+        time.sleep(5)
 
-time.sleep(5)
-driver.find_element_by_xpath('//*[@id="idDiv_SAOTCS_Proofs"]/div[1]/div').click()
-time.sleep(5)
-# Click on Next button
+        actions = ActionChains(driver)
+        actions.move_by_offset(0, 285).click().perform()
+        time.sleep(5)
+        driver.find_element_by_xpath('//*[@id="5"]').click()
+        time.sleep(5)
+        driver.find_element_by_xpath('//*[@id="mat-dialog-2"]/export-data-dialog/mat-dialog-actions/button[1]').click()
+        time.sleep(5)
 
-# Wait for two-factor authentication page to load and enter code
-WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.ID, "idTxtBx_SAOTCC_OTC")))
+        actions = ActionChains(driver)
+        actions.move_by_offset(700, -55).click().perform()
+        time.sleep(5)
+        driver.find_element_by_xpath('//*[@id="5"]').click()
+        time.sleep(5)
+        driver.find_element_by_xpath('//*[@id="mat-dialog-3"]/export-data-dialog/mat-dialog-actions/button[1]').click()
+        time.sleep(15)
 
-code = input("Enter two-factor authentication code: ")
-code_input = driver.find_element_by_id("idTxtBx_SAOTCC_OTC")
-code_input.send_keys(code)
+        actions = ActionChains(driver)
+        actions.move_by_offset(0, -341).click().perform()
+        time.sleep(5)
+        driver.find_element_by_xpath('//*[@id="5"]').click()
+        time.sleep(5)
+        driver.find_element_by_xpath('//*[@id="mat-dialog-4"]/export-data-dialog/mat-dialog-actions/button[1]').click()
+        time.sleep(5)
 
-# Click on Verify button
-driver.find_element_by_id("idSubmit_SAOTCC_Continue").click()
-time.sleep(10)
-driver.find_element_by_id("idSIButton9").click()
-time.sleep(10)
+        driver.find_element_by_xpath('//*[@id="content"]/div/report/exploration-container/div/div/docking-container/div/div/exploration-fluent-navigation/section/mat-action-list/button[2]').click()
 
+        time.sleep(10)
 
-actions = ActionChains(driver)
-actions.move_by_offset(949, 128).click().perform()
-time.sleep(5)
-driver.find_element_by_xpath('//*[@id="5"]').click()
+        actions = ActionChains(driver)
+        actions.move_by_offset(-696, 0).click().perform()
+        time.sleep(10)
+        driver.find_element_by_xpath('//*[@id="5"]').click()
+        time.sleep(10)
+        driver.find_element_by_xpath('//*[@id="mat-dialog-5"]/export-data-dialog/mat-dialog-actions/button[1]').click()
+        time.sleep(10)
 
-time.sleep(5)
-driver.find_element_by_xpath('//*[@id="mat-dialog-0"]/export-data-dialog/mat-dialog-actions/button[1]').click()
+        actions = ActionChains(driver)
+        actions.move_by_offset(0, 111).click().perform()
+        time.sleep(5)
+        driver.find_element_by_xpath('//*[@id="5"]').click()
+        time.sleep(5)
+        driver.find_element_by_xpath('//*[@id="mat-dialog-6"]/export-data-dialog/mat-dialog-actions/button[1]').click()
+        time.sleep(5)
 
-time.sleep(10)
-# Close the browser
-driver.quit()
+        actions = ActionChains(driver)
+        actions.move_by_offset(0, 285).click().perform()
+        time.sleep(5)
+        driver.find_element_by_xpath('//*[@id="5"]').click()
+        time.sleep(5)
+        driver.find_element_by_xpath('//*[@id="mat-dialog-7"]/export-data-dialog/mat-dialog-actions/button[1]').click()
+        time.sleep(5)
+
+        actions = ActionChains(driver)
+        actions.move_by_offset(695, -58).click().perform()
+        time.sleep(5)
+        driver.find_element_by_xpath('//*[@id="5"]').click()
+        time.sleep(5)
+        driver.find_element_by_xpath('//*[@id="mat-dialog-8"]/export-data-dialog/mat-dialog-actions/button[1]').click()
+        time.sleep(15)
+
+        actions = ActionChains(driver)
+        actions.move_by_offset(0, -341).click().perform()
+        time.sleep(5)
+        driver.find_element_by_xpath('//*[@id="5"]').click()
+        time.sleep(5)
+        driver.find_element_by_xpath('//*[@id="mat-dialog-9"]/export-data-dialog/mat-dialog-actions/button[1]').click()
+        time.sleep(5)
+        driver.quit()
---
+++ src/main.py
@@ -0,0 +1,34 @@
+from crawler import Crawler
+import os
+import argparse
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--email')
+    parser.add_argument('--password')
+    parser.add_argument('--group_id')
+    parser.add_argument('--report_id1')
+    parser.add_argument('--report_id2')
+
+    args = parser.parse_args()
+
+    email = args.email
+    password = args.password
+
+    report_id1 = args.report_id1
+    group_id = args.group_id
+    report_id2 = args.report_id2
+
+    crawl = Crawler(username=email, password=password, group_id=group_id, report_id=report_id2)
+
+    crawl.crawl()
+    
+    crawl = Crawler(username=email, password=password, group_id=group_id, report_id=report_id1)
+
+    crawl.crawl()
+
+    
+
+
+#python src/main.py --email pbi@in516ht.com --password 7rTMTgw#BFBPR*WU --group_id 418fc146-6f6a-4b64-afc8-d8856a0d5b6f --report_id1 745405bd-c702-481b-9e5d-bb3dfc1cc93d --report_id2 fc9354f1-c134-41e9-a443-b1276108b23f
